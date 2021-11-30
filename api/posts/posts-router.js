34a467bf22040c8d76a037f24f02441554b27a2b
jaredhall18 <jaredhall18@gmail.com>
2021-11-30 14:40:31 -0700

    Get post by id working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -20,7 +20,15 @@ router.get('/', async (req, res) => {
 // #### 2 [GET] /api/posts/:id
 router.get('/:id', async (req, res) => {
     try{
-        throw new Error("testing catch")
+        // throw new Error("testing catch")
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            res.status(200).json(post)
+        }
     } catch (err) {
         res.status(500).json({
             message: "The post information could not be retrieved"
